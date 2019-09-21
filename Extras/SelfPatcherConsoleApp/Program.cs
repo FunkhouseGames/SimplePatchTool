abc5b4852c269831535b9b54bb5b18766ab8b9d6
jmvogt <jordanmvogt@gmail.com>
2019-09-20 23:11:17 -0700

    Build multiple targets for SelfPatcherConsoleApp

--- Extras/SelfPatcherConsoleApp/Program.cs
+++ Extras/SelfPatcherConsoleApp/Program.cs
@@ -1,43 +1,35 @@
 ﻿using SelfPatcherCore;
 using System;
 
-namespace SelfPatcherConsoleApp
-{
-	public class Program
-	{
-		private class ConsoleSelfPatcherListener : ISelfPatcherListener
-		{
-			public SelfPatcher selfPatcher;
+namespace SelfPatcherConsoleApp {
+    public class Program {
+        private class ConsoleSelfPatcherListener : ISelfPatcherListener {
+            public SelfPatcher selfPatcher;
 
-			public void OnLogAppeared( string message )
-			{
-				Console.WriteLine( message );
-			}
+            public void OnLogAppeared(string message) {
+                Console.WriteLine(message);
+            }
 
-			public void OnProgressChanged( int currentInstruction, int numberOfInstructions )
-			{
-				Console.WriteLine( (int) ( currentInstruction * 100f / numberOfInstructions ) + "%" );
-			}
+            public void OnProgressChanged(int currentInstruction, int numberOfInstructions) {
+                Console.WriteLine((int)(currentInstruction * 100f / numberOfInstructions) + "%");
+            }
 
-			public void OnFail( string message )
-			{
-				Console.WriteLine( message );
-				Console.ReadKey( true );
-			}
+            public void OnFail(string message) {
+                Console.WriteLine(message);
+                Console.ReadKey(true);
+            }
 
-			public void OnSuccess()
-			{
-				selfPatcher.ExecutePostSelfPatcher();
-			}
-		}
+            public void OnSuccess() {
+                selfPatcher.ExecutePostSelfPatcher();
+            }
+        }
 
-		public static void Main( string[] args )
-		{
-			ConsoleSelfPatcherListener listener = new ConsoleSelfPatcherListener();
-			SelfPatcher selfPatcher = new SelfPatcher( listener );
-			listener.selfPatcher = selfPatcher;
+        public static void Main(string[] args) {
+            ConsoleSelfPatcherListener listener = new ConsoleSelfPatcherListener();
+            SelfPatcher selfPatcher = new SelfPatcher(listener);
+            listener.selfPatcher = selfPatcher;
 
-			selfPatcher.Run( args );
-		}
-	}
+            selfPatcher.Run(args);
+        }
+    }
 }
